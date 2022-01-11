8c64aaf5ddad16e42c60828fbc8636cd5e4b234c
Thomas Bui <thomasbui198@gmail.com>
2022-01-11 14:12:40 -0800

    Code now check if hand is moving left or right

--- index.js
+++ index.js
@@ -1,195 +1,66 @@
-// import DeviceDetector from "https://cdn.skypack.dev/device-detector-js@2.2.10";
-
-// // getting global variables
-// const mpHands = window;
-// const drawingUtils = window;
-// const controls = window;
-// const controls3d = window;
-
-// // Usage: testSupport({client?: string, os?: string}[])
-// // Client and os are regular expressions.
-// // See: https://cdn.jsdelivr.net/npm/device-detector-js@2.2.10/README.md for
-// // legal values for client and os
-// testSupport([
-//     { client: 'Chrome' },
-// ]);
-
-// function testSupport(supportedDevices) {
-//     const deviceDetector = new DeviceDetector();
-//     const detectedDevice = deviceDetector.parse(navigator.userAgent);
-//     let isSupported = false;
-//     for (const device of supportedDevices) {
-//         if (device.client !== undefined) {
-//             const re = new RegExp(`^${device.client}$`);
-//             if (!re.test(detectedDevice.client.name)) {
-//                 continue;
-//             }
-//         }
-//         if (device.os !== undefined) {
-//             const re = new RegExp(`^${device.os}$`);
-//             if (!re.test(detectedDevice.os.name)) {
-//                 continue;
-//             }
-//         }
-//         isSupported = true;
-//         break;
-//     }
-//     if (!isSupported) {
-//         alert(`This demo, running on ${detectedDevice.client.name}/${detectedDevice.os.name}, ` +
-//             `is not well supported at this time, continue at your own risk.`);
-//     }
-// }
-
-// // Our input frames will come from here.
-// const videoElement = document.getElementsByClassName('input_video')[0];
-// const canvasElement = document.getElementsByClassName('output_canvas')[0];
-// const controlsElement = document.getElementsByClassName('control-panel')[0];
-// const canvasCtx = canvasElement.getContext('2d');
-// const config = { locateFile: (file) => {
-//     return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${mpHands.VERSION}/${file}`;
-// } };
-
-// // We'll add this to our control panel later, but we'll save it here so we can
-// // call tick() each time the graph runs.
-// const fpsControl = new controls.FPS();
-// // Optimization: Turn off animated spinner after its hiding animation is done.
-// const spinner = document.querySelector('.loading');
-// spinner.ontransitionend = () => {
-//     spinner.style.display = 'none';
-// };
-
-// function onResults(results) {
-//     // Hide the spinner.
-//     document.body.classList.add('loaded');
-//     // Update the frame rate.
-//     fpsControl.tick();
-//     // Draw the overlays.
-//     canvasCtx.save();
-//     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-//     canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
-//     if (results.multiHandLandmarks && results.multiHandedness) {
-//         for (let index = 0; index < results.multiHandLandmarks.length; index++) {
-//             const classification = results.multiHandedness[index];
-//             const isRightHand = classification.label === 'Right';
-//             const landmarks = results.multiHandLandmarks[index];
-//             drawingUtils.drawConnectors(canvasCtx, landmarks, mpHands.HAND_CONNECTIONS, { color: isRightHand ? '#00FF00' : '#FF0000' });
-//             drawingUtils.drawLandmarks(canvasCtx, landmarks, {
-//                 color: isRightHand ? '#00FF00' : '#FF0000',
-//                 fillColor: isRightHand ? '#FF0000' : '#00FF00',
-//                 radius: (data) => {
-//                     return drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1);
-//                 }
-//             });
-//         }
-//     }
-//     canvasCtx.restore();
-//     if (results.multiHandWorldLandmarks) {
-//         // We only get to call updateLandmarks once, so we need to cook the data to
-//         // fit. The landmarks just merge, but the connections need to be offset.
-//         const landmarks = results.multiHandWorldLandmarks.reduce((prev, current) => [...prev, ...current], []);
-//         const colors = [];
-//         let connections = [];
-//         for (let loop = 0; loop < results.multiHandWorldLandmarks.length; ++loop) {
-//             const offset = loop * mpHands.HAND_CONNECTIONS.length;
-//             const offsetConnections = mpHands.HAND_CONNECTIONS.map((connection) => [connection[0] + offset, connection[1] + offset]);
-//             connections = connections.concat(offsetConnections);
-//             const classification = results.multiHandedness[loop];
-//             colors.push({
-//                 list: offsetConnections.map((unused, i) => i + offset),
-//                 color: classification.label,
-//             });
-//         }
-//     }
-//     else {
-//     }
-// }
-
-// let myConfig = {
-//     selfieMode: true,
-//     maxNumHands: 2,
-//     modelComplexity: 1,
-//     minDetectionConfidence: 0.5,
-//     minTrackingConfidence: 0.5
-// }
-
-// const hands = new mpHands.Hands(config);
-// hands.onResults(onResults);
-
-// // Present a control panel through which the user can manipulate the solution
-// // options.
-// new controls
-//     .ControlPanel(controlsElement, {
-//     selfieMode: true,
-//     maxNumHands: 2,
-//     modelComplexity: 1,
-//     minDetectionConfidence: 0.5,
-//     minTrackingConfidence: 0.5
-// })
-//     .add([
-//     new controls.StaticText({ title: 'MediaPipe Hands' }),
-//     fpsControl,
-//     new controls.Toggle({ title: 'Selfie Mode', field: 'selfieMode' }),
-//     new controls.SourcePicker({
-//         onFrame: async (input, size) => {
-//             const aspect = size.height / size.width;
-//             let width, height;
-//             if (window.innerWidth > window.innerHeight) {
-//                 height = window.innerHeight;
-//                 width = height / aspect;
-//             }
-//             else {
-//                 width = window.innerWidth;
-//                 height = width * aspect;
-//             }
-//             canvasElement.width = width;
-//             canvasElement.height = height;
-//             await hands.send({ image: input });
-//         },
-//     }),
-//     new controls.Slider({
-//         title: 'Max Number of Hands',
-//         field: 'maxNumHands',
-//         range: [1, 4],
-//         step: 1
-//     }),
-//     new controls.Slider({
-//         title: 'Model Complexity',
-//         field: 'modelComplexity',
-//         discrete: ['Lite', 'Full'],
-//     }),
-//     new controls.Slider({
-//         title: 'Min Detection Confidence',
-//         field: 'minDetectionConfidence',
-//         range: [0, 1],
-//         step: 0.01
-//     }),
-//     new controls.Slider({
-//         title: 'Min Tracking Confidence',
-//         field: 'minTrackingConfidence',
-//         range: [0, 1],
-//         step: 0.01
-//     }),
-// ])
-//     .on(x => {
-//     const options = x;
-//     videoElement.classList.toggle('selfie', options.selfieMode);
-//     hands.setOptions(options);
-// });
-
-
 const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-const canvasCtx = canvasElement.getContext('2d');
+// const canvasElement = document.getElementsByClassName('output_canvas')[0];
+// const canvasCtx = canvasElement.getContext('2d');
+const messageElem = document.getElementById("message")
+const WRIST_INDEX = 0
+
+let prevHandCoord = undefined
+let isRunning = true
 
 function onResults(results) {
+  if (!isRunning) return
+  // multiHandLandmarks contains up to 2 arrays representing two hands
+  // each element represents 1 hand and contains an arrays of landmarks 
+  /**
+   * multiHandLandmarks = [
+   *  [ // hand 1
+   *    {x, y, z}  // a landmark
+   *  ] 
+   * ]
+   */
+  let txt = "NONE";
+  if (results.multiHandLandmarks && results.multiHandLandmarks.length != 0) {
+    let hand = results.multiHandLandmarks[0]
+    if (prevHandCoord) {
+      try {
+        let delta = hand[WRIST_INDEX].x - prevHandCoord[WRIST_INDEX].x
+
+        // round to 2 decimal place, see https://stackoverflow.com/a/11832950/11683637
+        delta = Math.round(delta * 100) / 100
+        console.log(delta)
+
+        if (delta == 0) txt = "STAY"
+        else if (delta > 0) txt = "RIGHT"
+        else txt = "LEFT"
+      }
+      catch(e) {
+        console.log("Multi handlandmarks: ", results.multiHandLandmarks)
+        console.log("Hand: ", hand)
+        console.log("prevHandCoord", prevHandCoord)
+      }
+    }
+
+    prevHandCoord = hand
+  }
+  messageElem.textContent = txt;
+}
+
+document.getElementById("runningCheckbox").onclick = toggleIsRunning;
+function toggleIsRunning() {
+  isRunning = !isRunning;
+}
+
+function drawHandsDetected(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
   canvasCtx.drawImage(
       results.image, 0, 0, canvasElement.width, canvasElement.height);
   if (results.multiHandLandmarks) {
-    for (const landmarks of results.multiHandLandmarks) {
+    // draw out the detection spots
+    for (const landmarks of multiHandLandmarks) {
+      // functions below taken from drawing-utils HTML script
       drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-                     {color: '#00FF00', lineWidth: 5});
+                      {color: '#00FF00', lineWidth: 5});
       drawLandmarks(canvasCtx, landmarks, {color: '#FF0000', lineWidth: 2});
     }
   }
@@ -199,19 +70,25 @@ function onResults(results) {
 const hands = new Hands({locateFile: (file) => {
   return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
 }});
+
 hands.setOptions({
-  maxNumHands: 2,
+  maxNumHands: 1, // only need one hand
   modelComplexity: 1,
   minDetectionConfidence: 0.5,
   minTrackingConfidence: 0.5
 });
+
+// set callbacks whenever a result is passed
 hands.onResults(onResults);
 
+// init the camera
+// taken from the camera util script
+// this relies on having a video element
 const camera = new Camera(videoElement, {
   onFrame: async () => {
     await hands.send({image: videoElement});
   },
-  width: 1280,
-  height: 720
+  width: 128,
+  height: 72
 });
 camera.start();
